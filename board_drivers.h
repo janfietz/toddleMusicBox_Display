aa35347b33adcfdaacd9a26d55a96214c0f5be88
janfietz <janfietz@gmail.com>
2015-10-29 21:11:30 +0100

    cleanup

--- board_drivers.h
+++ board_drivers.h
@@ -8,12 +8,6 @@
 #ifndef _BOARD_DRIVERS_H_
 #define _BOARD_DRIVERS_H_
 
-#include "ch.h"
-#include "hal.h"
-#include "ws281x.h"
-
-static ws2811Config ws2811_cfg;
-
 void BoardDriverInit(void);
 void BoardDriverStart(void);
 void BoardDriverShutdown(void);
